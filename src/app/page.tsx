b2e77cf61c3dacb57c32532b4f6e2f17925c6ab7
Matthew Wang <wmatthew123@gmail.com>
2025-08-22 20:57:54 +0800

    add link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,31 @@ import { Suspense } from "react";
 
 export default function Home() {
   return (
-    <div className="w-screen">
-      <div className="relative  m-10">
-        {/* <Zoom> */}
-        <Box size={400} placement="bottom" depth={0} />
-        {/* </Zoom> */}
+    <div className="w-full p-10">
+      <div className="relative max-w-3xl mx-auto w-full">
+        <Box placement="bottom" depth={0} />
       </div>
+      {/* Github Repo */}
+      <a
+        href="https://github.com/matowang/recursive-server-component"
+        className="bottom-10 fixed text-white flex items-center gap-2 justify-center"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" />
+          <path d="M9 18c-4.51 2-5-2-7-2" />
+        </svg>
+        GitHub Repository
+      </a>
     </div>
   );
 }
@@ -15,7 +34,6 @@ export default function Home() {
 const RATIO = 1 / 1.61803398875;
 
 async function Box(props: {
-  size: number;
   placement: "top" | "bottom" | "left" | "right";
   depth: number;
 }) {
@@ -23,12 +41,10 @@ async function Box(props: {
     return null;
   }
   await new Promise((resolve) => setTimeout(resolve, 500));
-  console.log(props.placement, props.size);
   return (
     <div
       style={{
-        width: props.size,
-        height: props.size,
+        width: `${100 * RATIO}%`,
         ...{
           top: {
             top: 0,
@@ -52,11 +68,10 @@ async function Box(props: {
           },
         }[props.placement],
       }}
-      className="absolute outline outline-white/50 border-white animate-fade-in"
+      className="absolute outline outline-white/50 border-white animate-fade-in aspect-square"
     >
       <Suspense fallback={null}>
         <Box
-          size={props.size * RATIO}
           placement={
             (
               {
@@ -71,24 +86,19 @@ async function Box(props: {
         />
       </Suspense>
       <div className="absolute inset-0 border border-white text-white overflow-clip">
-        <Circle size={props.size * 2} placement={props.placement} />
+        <Circle placement={props.placement} />
       </div>
     </div>
   );
 }
 
-function Circle(props: {
-  size: number;
-  placement: "top" | "bottom" | "left" | "right";
-}) {
-  console.log("C", props.placement, props.size);
+function Circle(props: { placement: "top" | "bottom" | "left" | "right" }) {
+  console.log("C", props.placement);
   return (
     <div
-      className="rounded-full border border-white absolute overflow-clip"
+      className="rounded-full border border-white absolute overflow-clip aspect-square"
       style={{
-        width: `${props.size}px`,
-        height: `${props.size}px`,
-
+        width: `200%`,
         ...{
           top: {
             right: 0,
