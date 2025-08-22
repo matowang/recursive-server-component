a6cadcc43da92e1d3d917b22e8f6958ca2b6671a
Matthew Wang <wmatthew123@gmail.com>
2025-08-22 21:18:14 +0800

    refresh button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,6 +27,26 @@ export default function Home() {
         </svg>
         GitHub Repository
       </a>
+      {/* Refresh Button */}
+      <form className="fixed bottom-10 right-10">
+        <button type="submit" className="text-white flex items-center gap-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path d="M3 12a9 9 0 1 0 9-9 9.75 9.75 0 0 0-6.74 2.74L3 8" />
+            <path d="M3 3v5h5" />
+          </svg>
+          Refresh
+        </button>
+      </form>
     </div>
   );
 }
@@ -93,7 +113,6 @@ async function Box(props: {
 }
 
 function Circle(props: { placement: "top" | "bottom" | "left" | "right" }) {
-  console.log("C", props.placement);
   return (
     <div
       className="rounded-full border border-white absolute overflow-clip aspect-square"
